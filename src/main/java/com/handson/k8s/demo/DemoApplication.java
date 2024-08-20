4f5af06d81729ea89e393d650d98902ce2686ba2
Khandekar Saleh <msnishan@gmail.com>
2024-08-20 10:11:51 +0530

    adding client service

--- src/main/java/com/handson/k8s/demo/DemoApplication.java
+++ src/main/java/com/handson/k8s/demo/DemoApplication.java
@@ -2,11 +2,14 @@ package com.handson.k8s.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +19,8 @@ import java.util.Map;
 public class DemoApplication {
 
 	private final Map<String, Student> studentMap = new HashMap<>();
+
+	RestTemplate restTemplate = new RestTemplate();
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -30,9 +35,14 @@ public class DemoApplication {
 
 	@GetMapping("/student/{id}")
 	public Student getStudentById(@PathVariable String id) {
-		return studentMap.get(id);
+		ResponseEntity<Address> address =
+				restTemplate.getForEntity("http://client-service/address/" + id, Address.class);
+		Student s = studentMap.get(id);
+		return new Student(s.id, s.name, address.getBody());
 	}
 
-	public record Student(String id, String name) {}
+	public record Student(String id, String name, Address address) {}
+
+	public record Address(String stdId, String city, String country) {}
 
 }
