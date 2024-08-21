783da0a42324138a178bc3e70f3ffc4b4458d089
Khandekar Saleh <msnishan@gmail.com>
2024-08-21 09:53:43 +0530

    svc post example

--- src/main/java/com/handson/k8s/demo/DemoApplication.java
+++ src/main/java/com/handson/k8s/demo/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.handson.k8s.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientSsl;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	private final Map<String, Student> studentMap = new HashMap<>();
 
 	RestTemplate restTemplate = new RestTemplate();
@@ -30,6 +37,11 @@ public class DemoApplication {
 	@PostMapping("/student")
 	public Student createStudent(@RequestBody Student student) {
 		studentMap.put(student.id, student);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+		HttpEntity<Address> httpEntity = new HttpEntity<>(student.address, headers);
+		restTemplate.postForEntity("http://client-service/address", httpEntity, Address.class);
 		return student;
 	}
 
